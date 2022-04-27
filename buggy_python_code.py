859ba01f76a5099068cbe592c43e35f5e9428aea
unknown <xmaca@ad.fi.muni.cz>
2022-04-27 12:50:37 +0200

    Fixed some buggs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -8,3 +8,31 @@ import subprocess
 def transcode_file(filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
+
+
+"""Assert statements"""
+def function(user):
+    assert user.is_admin, 'user does not have access'
+    # secure code...
+
+
+"""Pickles"""
+class RunBinSh():
+    """reduce"""
+    def __reduce__(self):
+        return (subprocess.Popen, (('/bin/sh',),))
+    """reduce"""
+    def __reduce2__(self):
+        return (subprocess.Popen, (('/bin/sh',),))
+
+
+"""docstring"""
+def import_urlib_version(version):
+    exec("import urllib%s as urllib" % version)
+
+
+"""docstring"""
+def index():
+    """docstring"""
+    module = flask.request.args.get("module")
+    import_urlib_version(module)

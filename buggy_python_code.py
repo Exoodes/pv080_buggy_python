91580fcf56d4a6f1c8ac34209caf3e00d4a90653
unknown <xmaca@ad.fi.muni.cz>
2022-04-27 12:51:19 +0200

    Fixed some buggs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,38 +1,41 @@
-"""Buggy code"""
+""" Buggy code """
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-
 # gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+import base64
 import subprocess
+import flask
 
-"""docstring"""
+""" docstring """
 def transcode_file(filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
-"""Assert statements"""
+""" Assert statements """
 def function(user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
-"""Pickles"""
+""" Pickles """
 class RunBinSh():
-    """reduce"""
+    """ reduce """
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
-    """reduce"""
+    """ reduce """
     def __reduce2__(self):
         return (subprocess.Popen, (('/bin/sh',),))
 
-
-"""docstring"""
+""" docstring """
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
 
-
-"""docstring"""
+""" docstring """
 def index():
-    """docstring"""
+    """ docstring """
     module = flask.request.args.get("module")
     import_urlib_version(module)
+
+
+print(base64.b64encode(pickle.dumps(RunBinSh())))

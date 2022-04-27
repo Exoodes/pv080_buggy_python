8bb161662e40b494d1295a797914472a39b2f1b1
unknown <xmaca@ad.fi.muni.cz>
2022-04-27 13:07:22 +0200

    Fixed some buggs

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,39 +1,33 @@
-""" Buggy code """
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-
-# gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+import cPickle
+import subprocess
 import base64
 import subprocess
 import flask
 
-""" docstring """
-def transcode_file(filename):
+# Input injection
+def transcode_file(request, filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
-""" Assert statements """
-def function(user):
+# Assert statements
+def foo(request, user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
 
-""" Pickles """
-class RunBinSh():
-    """ reduce """
+# Pickles
+class RunBinSh(object):
     def __reduce__(self):
         return (subprocess.Popen, (('/bin/sh',),))
-    """ reduce """
-    def __reduce2__(self):
-        return (subprocess.Popen, (('/bin/sh',),))
 
-""" docstring """
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
 
-""" docstring """
+@app.route('/')
 def index():
-    """ docstring """
     module = flask.request.args.get("module")
     import_urlib_version(module)
 
